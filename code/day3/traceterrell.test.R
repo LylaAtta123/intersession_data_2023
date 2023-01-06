81a9e4c1b258806d7fb99eb1a9db61652b776eca
Trace <tterrel6@jh.edu>
2023-01-06 08:14:16 -0800

    Trace's Rscript test

---
+++ code/day3/traceterrell.test.R
@@ -0,0 +1,14 @@
+#### SETUP ####
+
+# install.packages('datasets')
+# install.packages('viridis')
+library(datasets)
+library(viridis)
+
+#### DATA ####
+data('iris')
+class(iris)
+head(iris)
+
+#### OTHER NOTES ####
+#install.packages('datasets', dependencies = TRUE)
