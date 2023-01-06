ffc99a33487fec0326b81295fe0f4873151ba48d
kpclifton <kalclif@cox.net>
2023-01-06 11:08:34 -0500

    update basics

--- week1/content_day3/basics_examples.R
+++ week1/content_day3/basics_examples.R
@@ -9,6 +9,7 @@ L3 <- LETTERS[1:3]
 #lists
 my.list <- c(3, 6, 25)
 x <- c(0:10, 50)
+x[3] #indexing start at 1
 
 #functions
 mean(my.list)
@@ -16,6 +17,8 @@ char <- sample(L3, 10, replace = TRUE)
 
 #functions documentation
 ?mean
+help(t.test)
 
 #data frames
+d <- data.frame(x = 1, y = 1:10)
 d <- data.frame(x = 1, y = 1:10, char = char)
