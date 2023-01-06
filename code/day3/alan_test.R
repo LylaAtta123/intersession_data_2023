0fca0898c8190497735dfd056aa7cd3cacb4a8fa
Alan Mao <52225670+am12@users.noreply.github.com>
2023-01-06 11:14:39 -0500

    rstudio tutorial

---
+++ code/day3/alan_test.R
@@ -0,0 +1,15 @@
+# cmd-shift-C to group comment
+
+### SETUP ###
+# install.packages('datasets')
+# install.packages('viridis')
+library(datasets) #loads the package
+library(viridis)
+
+### DATA ###
+data("iris") #load the iris dataset
+
+class(iris) #previews the iris
+head(iris)
+
+getwd()
